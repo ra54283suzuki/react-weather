dd8b7f23b2e62c5faae1fdc81879c70241fbd00c
ra54283suzuki <ra54283suzuki@gmail.com>
2022-05-31 11:47:05 +0900

    Fixing Index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Title from "./components/Title";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>こんにちは</h1>
+      <Title />
     </div>
   );
-}
+};
 
 export default App;
