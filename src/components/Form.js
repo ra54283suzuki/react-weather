521e82c7f3a1673e321c1ac41693f033cfcb93f0
ra54283suzuki <ra54283suzuki@gmail.com>
2022-06-02 23:45:36 +0900

    Creating Form.js ②.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Form = () => {
   const [city, setCity] = useState("");
+  const getWeather = (e) => {
+    e.preventDefault();
+    axios.get("https://api.weatherapi.com/v1/current.json?key=cc2a673266714814849141004220206&q=London&aqi=no").then(res=>console.log(res))
+  }
   return (
     <form>
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 };
